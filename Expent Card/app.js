eb755cc49530a452d163a35b46b5e4255c853e79
Tanvirmehedi <tmjishan201@gmail.com>
2021-11-01 16:22:54 +0600

    Expent Card

--- Expent Card/app.js
+++ Expent Card/app.js
@@ -1,11 +1,5 @@
 const panels = document.querySelectorAll(".panel");
 
-// for (let i = 0; i <= panels.length; i++) {
-//     panels[i].addEventListener("click", () => {
-//         // removeActiveClass();
-//         panels[i].classList.add("active");
-//     })
-// }
 
 panels.forEach((panel) => {
     panel.addEventListener("click", () => {
