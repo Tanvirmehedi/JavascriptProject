4c4ee00b57357c5c66e09f92f3101834f76099e0
Tanvirmehedi <tmjishan201@gmail.com>
2021-11-04 11:09:21 +0600

    Hide Search Bar

--- Hide SearchBar/app.js
+++ Hide SearchBar/app.js
@@ -4,7 +4,7 @@
     Creator : Tanvir Jishan,
     version : 1.0,
     GitHub Project Link : https://github.com/Tanvirmehedi/JavascriptProject,
-    GitHub : https://github.com/Tanvirmehedi,
+    GitHub : https://github.com/Tanvirmehedi, 
     Linkedin :  www.linkedin.com/in/tanvirjisn
 */
 const title = document.title = 'Hide Search Bar';
