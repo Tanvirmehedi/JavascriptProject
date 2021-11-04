070027402bddf6e2eb15a73a2d9813cecd2b55e0
Tanvirmehedi <tmjishan201@gmail.com>
2021-11-04 10:55:07 +0600

    Blank Project Template

---
+++ _project_blank/app.js
@@ -0,0 +1,10 @@
+/*
+    Project Name : Main App,
+    Date : 04/11/2021,
+    Creator : Tanvir Jishan,
+    version : 1.0,
+    GitHub Project Link : https://github.com/Tanvirmehedi/JavascriptProject,
+    GitHub : https://github.com/Tanvirmehedi,
+    Linkedin :  www.linkedin.com/in/tanvirjisn
+*/
+document.title = 'Main File';
