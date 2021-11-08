629ea79c4e504534e62f58cad287b8dd8e95bbfb
Tanvirmehedi <tmjishan201@gmail.com>
2021-11-08 12:04:58 +0600

    Blank Template With Bootstrap and Cdn

---
+++ _project_blank - Bootstrap/app.js
@@ -0,0 +1,13 @@
+/*
+    Project Name : Main App Bootstrap,
+    Date : 04/11/2021,
+    Creator : Tanvir Jishan,
+    version : 1.0,
+    GitHub Project Link : https://github.com/Tanvirmehedi/JavascriptProject,
+    GitHub : https://github.com/Tanvirmehedi,
+    Linkedin :  www.linkedin.com/in/tanvirjisn
+*/
+const title = document.title = 'Main App Bootstrap';
+const h1 = document.querySelector('main h1');
+
+h1.textContent = title;
